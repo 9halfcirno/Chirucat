596510ec821b59233cc0023b6ec711ec0ae732d6
9halfcirno <baka9cirno@outlook.com>
2026-08-26 10:47:47 +0800

    杂项修改 - 删除旧的测试文件 - 更新测试用例 - 更新TODO

---
+++ TODO.md
@@ -0,0 +1,5 @@
+- [x] 将console全部换成`Looger`
+
+- 实现Core根据enable启动bot
+
+- ...
--- test.ts
+++ test.ts
@@ -1,28 +1,8 @@
-import { readFileSync, readSync } from "fs";
-import { Bot } from "./src/bot/bot";
-import { AdapterContext } from "./src/plugin/contexts/adapter-context";
-import { PluginLoader } from "./src/plugin/loader";
-import { Plugin } from "./src/plugin/plugin";
-import { UserManager } from "./src/user-manager";
-import path from "path";
-import { SessionManager } from "./src/session-manager";
-import { log } from "console";
 import { Core } from "./src/core";
 
 const core = new Core();
-await core.start();
+await core.init();
 
 await core.bot.start("chirucat");
 
 const bot = core.bot.bots.get("chirucat")!;
-
-bot.plugin.load("qq-adapter")
-bot.plugin.load("ping")
-
-bot.command.register({
-	name: "test",
-	handler(message, args) {
-		console.log(args);
-		
-	},
-})
