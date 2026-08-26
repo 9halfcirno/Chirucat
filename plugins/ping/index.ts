3e9ed2b96759dc0202e771fa8fe228af4b4acdc1
9halfcirno <baka9cirno@outlook.com>
2026-08-26 11:42:45 +0800

    更新测试插件

--- plugins/ping/index.ts
+++ plugins/ping/index.ts
@@ -1,10 +1,12 @@
 import type { PluginContext } from "../../src/plugin/contexts/context"
+import Logger from "../../src/utils/logger";
+
+const logger = new Logger("ping");
 
 export default {
 	init(ctx: PluginContext) {
-		ctx.message.start("ping", (msg) => {
-			console.log(msg.text);
-			
+		ctx.command.register("ping", (msg) => {
+			msg?.reply("pong")
 		})
 	}
 }
