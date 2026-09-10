a18f332c5fefce5f479b3ecffa17a02746f75c5c
9halfcirno <baka9cirno@outlook.com>
2026-09-10 20:38:31 +0800

    消息打印优先拼接`msg.block`以提供完整信息

--- src/bot/message-handler.ts
+++ src/bot/message-handler.ts
@@ -1,4 +1,5 @@
 import type { Message } from "../entity/message";
+import type { MessageBlock } from "../protocols/message-block";
 import type { Bot } from "./bot";
 import { MessageFilter } from "./message-filter";
 
@@ -29,12 +30,34 @@ export class MessageHandler {
 		const uinfo = this.bot.core.user!.query(user.id);
 		const sinfo = this.bot.core.session!.query(session.id);
 
+		// 优先把消息块拼接成一段文本, 再随整条日志一次性打印
+		const content = this._concatBlocks(message.blocks) || message.text;
+
 		let str = "";
 		str += "[收]"
 		str += `[${platform} ${session.type}:${sinfo?.id || "unknown"}]${ok === false ? " 已过滤" : ""}\n`
-		str += `<${user.name}(${uinfo!.id})> ${message.text}`;
+		str += `<${user.name}(${uinfo!.id})> ${content}`;
 
 		this.bot.logger.log(str);
 		
+	}
+
+	/** 按顺序把消息块拼接为可读文本 */
+	private _concatBlocks(blocks: MessageBlock[]): string {
+		let text = "";
+		for (const block of blocks) {
+			switch (block.type) {
+				case "text":
+					text += block.text;
+					break;
+				case "mention":
+					text += `@${block.name || block.id}`;
+					break;
+				case "image":
+					text += `[图片: ${block.file || block.url}]`;
+					break;
+			}
+		}
+		return text;
 	}
 }
