9a361ea21908e3a7780d53263f19d4ed5c084e9a
9halfcirno <baka9cirno@outlook.com>
2026-09-20 19:42:57 +0800

    更新示例Ping插件, 添加示例配置, 移除des示例依赖 - 支持动态修改响应指令和内容 - 支持控制ping响应和处理延迟

--- plugins/ping/index.ts
+++ plugins/ping/index.ts
@@ -1,3 +1,4 @@
+import type { Message } from "../../src/entity/message";
 import type { PluginContext } from "../../src/plugin/contexts/context"
 import Logger from "../../src/utils/logger";
 
@@ -5,10 +6,32 @@ const logger = new Logger("ping");
 
 export default {
 	init(ctx: PluginContext) {
-		ctx.command.register("ping", (msg) => {
-			msg?.reply("pong")
+		let command = ctx.config.get("command") as string;
+		ctx.logger.log(`Ping指令: ${command}`)
+		let com = ctx.command.register(command, reply)
+
+		ctx.config.watch((k, n, o) => {
+			ctx.logger.log(k, n, o)
+			if (k === "command") {
+				if (o === n) return;
+				ctx.logger.log(`Ping指令变化: ${o} -> ${n}`)
+				ctx.command.unregister(com);
+				com = ctx.command.register(n as string, reply)
+			}
+
 		})
+
+		async function reply(msg: Message | null) {
+			if (ctx.config.get("enable") === false) return;
+
+			let offset = ctx.config.get<number>("offset");
+			if (offset >= 10) await sleep(offset); 
+			
+			msg?.reply(ctx.config.get("reply", "pong!"));
+		}
 
 		ctx.logger.log(ctx.require("des"))
 	}
 }
+
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
